371fa3958a4afdfa22dc6fda1365b71660ef1107
gyubot <gyubot@mrbak.net>
2023-01-03 15:34:17 +0900

    24시간 전 온도와 현재 온도를 비교하여 다음 조건에 맞는 문구를 반환하는 모듈의 테스트코드 작성

---
+++ weatherbot/domain-model/src/main/kotlin/weatherbot/templature/TemplatureDiffTextGenerator.kt
@@ -0,0 +1,9 @@
+package weatherbot.templature
+
+import weatherbot.WeatherHistorical
+
+class TemplatureDiffTextGenerator {
+    fun generate(current: WeatherHistorical, before: WeatherHistorical): String {
+        return ""
+    }
+}
---
+++ weatherbot/domain-model/src/test/kotlin/weatherbot/templature/TemplatureTextGeneratorTest.kt
@@ -0,0 +1,60 @@
+package weatherbot.templature
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import weatherbot.WeatherHistorical
+import weatherbot.greeting.WeatherHistoricalFixture
+
+class TemplatureTextGeneratorTest {
+    @ParameterizedTest
+    @MethodSource("getWeatherDatas")
+    fun generate_함수는_24시간전_날씨와_현재_날씨를_바탕으로_올바른_문구를_반환한다(
+        current: WeatherHistorical,
+        before24h: WeatherHistorical,
+        text: String
+    ) {
+        // Arrange
+        val sut = TemplatureDiffTextGenerator()
+
+        // Act
+        val actual = sut.generate(current = current, before = before24h)
+
+        // Assert
+        assertEquals(actual, text)
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun getWeatherDatas(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 15), WeatherHistoricalFixture.create(temp = 16),
+                    "어제보다 1도 덜 덥습니다."
+                ),
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 14), WeatherHistoricalFixture.create(temp = 15),
+                    "어제보다 1도 더 춥습니다."
+                ),
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 15), WeatherHistoricalFixture.create(temp = 14),
+                    "어제보다 1도 더 덥습니다."
+                ),
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 14), WeatherHistoricalFixture.create(temp = 13),
+                    "어제보다 1도 덜 춥습니다."
+                ),
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 15), WeatherHistoricalFixture.create(temp = 15),
+                    "어제와 비슷하게 덥습니다."
+                ),
+                Arguments.of(
+                    WeatherHistoricalFixture.create(temp = 14), WeatherHistoricalFixture.create(temp = 14),
+                    "어제와 비슷하게 춥습니다."
+                ),
+            )
+        }
+    }
+}
